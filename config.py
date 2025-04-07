6f60cb4083a6254633049033f3b3e4df0d4ec8bc
unknown <shiekyaacop2005@gmail.com>
2025-04-07 21:25:05 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,14 @@
+train_path = "D:/ml proj/Video-Captioning-main/data/training_data"
+test_path = "D:/ml proj/Video-Captioning-main/data/testing_data"
+batch_size = 320
+learning_rate = 0.0007
+epochs = 150
+latent_dim = 512
+num_encoder_tokens = 4096
+num_decoder_tokens = 1500
+time_steps_encoder = 80
+max_probability = -1
+save_model_path = 'D:/ml proj/Video-Captioning-main/model_final1'
+validation_split = 0.15
+max_length = 10
+search_type = 'greedy'
